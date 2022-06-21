a0d9ce2f21639cd197327263361e407fe53a950c
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2022-06-21 10:13:48 +0400

    Enable smooth scrolling for all browsers

--- js/script.js
+++ js/script.js
@@ -10,6 +10,28 @@ btnNavEl.addEventListener('click', function () {
   headerEl.classList.toggle('nav-open');
 });
 
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+allLinks.forEach(function (link) {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+    const href = link.getAttribute('href');
+
+    // Scroll back to the top
+    if (href === '#') window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    // Scroll to other links
+    if (href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains('main-nav-link'))
+      headerEl.classList.toggle('nav-open');
+  });
+});
+
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
   var flex = document.createElement('div');
@@ -28,8 +50,6 @@ function checkFlexGap() {
   if (!isSupported) document.body.classList.add('no-flexbox-gap');
 }
 checkFlexGap();
-
-// https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
 /*
 .no-flexbox-gap .main-nav-list li:not(:last-child) {
