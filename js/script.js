27babe86333ee2652f145b875261bbc7e90539aa
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-02-09 10:49:46 +0400

    implement functionality for mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+// Set current year
 const yearEl = document.querySelector('.year');
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Mobile navigation functionality
+const headerEl = document.querySelector('header');
+const btnNavEl = document.querySelector('.btn-mobile-nav');
+
+btnNavEl.addEventListener('click', () => {
+  headerEl.classList.toggle('nav-open');
+});
